44e3c8b5637e493461e11f89872d858c2aadfa37
Dave Syer <dsyer@vmware.com>
2021-03-29 10:00:10 +0100

    Revert

--- default.nix
+++ default.nix
@@ -5,20 +5,6 @@ with pkgs;
 let
 
   kindSetup = pkgs.writeShellScriptBin "kind-setup" "./kind-setup.sh";
-  telepresence = pkgs.stdenv.mkDerivation {
-    pname = "telepresence2";
-    version = "2.0.1";
-    src = pkgs.fetchurl {
-      url = "https://app.getambassador.io/download/tel2/linux/amd64/latest/telepresence";
-      sha256 = "1ip77gfrx3ilqzksmvgphdhx9l0y2jl1xrfhx8zs3py7wn63986s";
-    };
-    phases = [ "installPhase" "patchPhase" ];
-    installPhase = ''
-      mkdir -p $out/bin
-      cp $src $out/bin/telepresence
-      chmod a+x $out/bin/telepresence
-    '';
-  };
 
 in buildEnv {
   name = "env";
