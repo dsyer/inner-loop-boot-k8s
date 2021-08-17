e79db9d2865521d05aa9e679965f982e0c217a1b
Dave Syer <dsyer@vmware.com>
2021-08-17 09:23:40 +0100

    Use telepresence recipe from nix

--- default.nix
+++ default.nix
@@ -5,21 +5,6 @@ with pkgs;
 let
 
   kindSetup = pkgs.writeShellScriptBin "kind-setup" "./kind-setup.sh";
-  telepresence = pkgs.stdenv.mkDerivation {
-    pname = "telepresence";
-    version = "2.4.0";
-    src = pkgs.fetchurl {
-      url =
-        "https://app.getambassador.io/download/tel2/linux/amd64/2.4.0/telepresence";
-      sha256 = "0qh6m5xghl9p59shfgm7ydx3psx00f11zbsqbn7xl6sz950ac0d6";
-    };
-    phases = [ "installPhase" ];
-    installPhase = ''
-      mkdir -p $out/bin
-      cp $src $out/bin/telepresence
-      chmod a+x $out/bin/telepresence
-    '';
-  };
 
 in buildEnv {
   name = "env";
@@ -32,7 +17,7 @@ in buildEnv {
     kustomize
     skaffold
     kindSetup
-    telepresence
+    telepresence2
     tilt
   ];
 }
